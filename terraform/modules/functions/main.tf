c0384b168e5de0dcfd2d22cc7f2eea2ef10537f1
Yesaswi Vadapalli <yesaswi.vadapalli@gmail.com>
2024-05-15 16:09:19 -0400

    Fixing bugs

--- terraform/modules/functions/main.tf
+++ terraform/modules/functions/main.tf
@@ -28,9 +28,9 @@ resource "google_cloudfunctions2_function" "process_audio_function" {
 
   service_config {
     max_instance_count  = 1
-    available_memory    = "512M"
-    available_cpu = "1"
-    timeout_seconds     = 60
+    available_memory    = "4GiB"
+    available_cpu       = "1"
+    timeout_seconds     = 3600
     service_account_email = var.service_account_email
     environment_variables = {
         AUDIO_BUCKET_NAME = var.audio_bucket_name
